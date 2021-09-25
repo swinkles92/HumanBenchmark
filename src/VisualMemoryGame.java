c14fdfe52f1b20af5e01071e8471b9ecf210a814
Samuel Winkles <swinkles92@unm.edu>
2021-09-25 17:05:39 -0600

    Completed Visual Memory Game. Aim Trainer, Typing, and Math Solver need work. All games need CSV functionality.

--- src/VisualMemoryGame.java
+++ src/VisualMemoryGame.java
@@ -1,3 +1,6 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -5,18 +8,32 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class VisualMemoryGame {
     private final Scene scene;
-    private final BorderPane root;
+    private final BorderPane startScreen;
+    private final BorderPane gameScreen;
+    private TilePane gameBoard;
+    private LinkedList<Rectangle> rectList = new LinkedList<>();
+    private LinkedList<Integer> gameSequence = new LinkedList<>();
+    private int score = 0;
+    private Label scoreLabel = new Label();
 
     public VisualMemoryGame(Scene scene, int SIZE, TilePane mainMenu) {
         this.scene = scene;
-        root = new BorderPane();
-
-        VBox vBox = new VBox(10);
+        startScreen = new BorderPane();
+        gameScreen = new BorderPane();
+        gameBoard = generateBoard(SIZE);
+        VBox startVBox = new VBox(10);
+        VBox gameVBox = new VBox(25);
 
         Label title = new Label("Visual Memory");
         title.setFont(new Font(40));
@@ -25,16 +42,98 @@ public class VisualMemoryGame {
                 "Click Start to begin.");
         gameDesc.setFont(new Font(20));
         gameDesc.setWrappingWidth(225);
-
         Button backButton = new Button("Back to Main Menu");
+        Button startBtn = new Button("Start");
+        startVBox.getChildren().addAll(title, gameDesc, startBtn, backButton);
+        startVBox.setAlignment(Pos.CENTER);
+        startScreen.setCenter(startVBox);
+
+        scoreLabel = new Label("Score: " + score);
+        scoreLabel.setFont(new Font(40));
+        scoreLabel.setAlignment(Pos.CENTER);
+        gameVBox.setAlignment(Pos.CENTER);
+        gameVBox.getChildren().addAll(scoreLabel, gameBoard, backButton);
+        gameScreen.setCenter(gameVBox);
+
+
         backButton.setOnAction(event -> {
             scene.setRoot(mainMenu);
         });
+        startBtn.setOnAction(event -> {
+            scene.setRoot(gameScreen);
+            gameLoop();
+        });
 
-        vBox.getChildren().addAll(title, gameDesc, backButton);
-        vBox.setAlignment(Pos.CENTER);
-
-        root.setCenter(vBox);
     }
-    public void show() { scene.setRoot(root); }
+    public TilePane generateBoard(int SIZE) {
+        TilePane gameBoard = new TilePane();
+        gameBoard.setAlignment(Pos.CENTER);
+        gameBoard.setVgap(2.5);
+        gameBoard.setHgap(2.5);
+        gameBoard.setMaxWidth(SIZE / 2);
+        for(int i = 0; i < 9; i++) {
+            int counter = i;
+            Rectangle rect = new Rectangle(SIZE / 6.5, SIZE / 6.5);
+            rect.setFill(Color.DIMGREY);
+            rect.setOnMouseClicked(event -> {
+                if(!gameSequence.isEmpty()) {
+                    if(gameSequence.contains(counter)) {
+                        gameSequence.removeFirstOccurrence(counter);
+                        Timeline t = new Timeline(
+                                new KeyFrame(Duration.seconds(0),
+                                        new KeyValue(rect.fillProperty(), Color.GREEN)),
+                                new KeyFrame(Duration.seconds(.5 - 0.001),
+                                        new KeyValue(rect.fillProperty(), Color.GREEN)),
+                                new KeyFrame(Duration.seconds(.5),
+                                        new KeyValue(rect.fillProperty(), Color.DIMGREY))
+                        );
+                        t.play();
+                        if(gameSequence.isEmpty()) {
+                            score++;
+                            scoreLabel.setText("Score: " + score);
+                            gameLoop();
+                        }
+                    }
+                    else scoreLabel.setText("Game over!");
+                }
+            });
+            rectList.add(rect);
+            gameBoard.getChildren().add(rect);
+        }
+        return gameBoard;
+    }
+    public void gameLoop() {
+        gameSequence.clear();
+        generateSequence();
+        for(int i = 0; i < gameSequence.size(); i++) {
+            int curr = gameSequence.get(i);
+            Timeline t = playAnimation(curr);
+            t.play();
+        }
+    }
+    public Timeline playAnimation(int curr) {
+        Timeline t = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                        new KeyValue(rectList.get(curr).fillProperty(), Color.GOLD)),
+                new KeyFrame(Duration.seconds(1.5 - 0.001),
+                        new KeyValue(rectList.get(curr).fillProperty(), Color.GOLD)),
+                new KeyFrame(Duration.seconds(1.5),
+                        new KeyValue(rectList.get(curr).fillProperty(), Color.DIMGREY))
+        );
+        return t;
+    }
+    public void generateSequence() {
+        int rand;
+        for(int i = 0; i < score + 1; i++) {
+            rand = ThreadLocalRandom.current().nextInt(0,9);
+            if(gameSequence.contains(rand)) {
+                while(gameSequence.contains(rand)) {
+                    rand = ThreadLocalRandom.current().nextInt(0,9);
+                }
+                gameSequence.push(rand);
+            }
+            else gameSequence.push(rand);
+        }
+    }
+    public void show() { scene.setRoot(startScreen); }
 }
