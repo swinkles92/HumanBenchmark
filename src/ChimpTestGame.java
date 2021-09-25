d9bfd6e0d780a027c8ee9e0448e6660c9f250ce2
Samuel Winkles <swinkles92@unm.edu>
2021-09-25 16:46:09 -0600

    Completed Chimp Test Game!

--- src/ChimpTestGame.java
+++ src/ChimpTestGame.java
@@ -1,23 +1,43 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ChimpTestGame {
     private final Scene scene;
-    private final BorderPane root;
+    private final BorderPane startScreen;
+    private final BorderPane gameScreen;
+    private TilePane gameBoard;
+    private LinkedList<Rectangle> rectList = new LinkedList<>();
+    private LinkedList<Label> labelsList = new LinkedList<>();
+    private LinkedList<Integer> gameSequence = new LinkedList<>();
+    private int score = 0;
+    private Label scoreLabel = new Label();
 
     public ChimpTestGame(Scene scene, int SIZE, TilePane mainMenu) {
         this.scene = scene;
-        root = new BorderPane();
+        startScreen = new BorderPane();
+        gameScreen = new BorderPane();
+        gameBoard = generateBoard(SIZE);
 
-        VBox vBox = new VBox(10);
-
+        VBox startVbox = new VBox(10);
         Label title = new Label("Chimp Test");
         title.setFont(new Font(40));
         title.setAlignment(Pos.CENTER);
@@ -25,16 +45,94 @@ public class ChimpTestGame {
                 "Click Start to begin.");
         gameDesc.setFont(new Font(20));
         gameDesc.setWrappingWidth(225);
-
         Button backButton = new Button("Back to Main Menu");
+        Button startButton = new Button("Start");
+        startVbox.getChildren().addAll(title, gameDesc, startButton, backButton);
+        startVbox.setAlignment(Pos.CENTER);
+        startScreen.setCenter(startVbox);
+
+        VBox gameVBox = new VBox(25);
+        scoreLabel = new Label("Score: " + score);
+        scoreLabel.setFont(new Font(40));
+        scoreLabel.setAlignment(Pos.CENTER);
+        gameVBox.setAlignment(Pos.CENTER);
+        gameVBox.getChildren().addAll(scoreLabel, gameBoard, backButton);
+        gameScreen.setCenter(gameVBox);
+
+        startButton.setOnAction(event -> {
+            scene.setRoot(gameScreen);
+            gameLoop();
+        });
         backButton.setOnAction(event -> {
             scene.setRoot(mainMenu);
         });
-
-        vBox.getChildren().addAll(title, gameDesc, backButton);
-        vBox.setAlignment(Pos.CENTER);
-
-        root.setCenter(vBox);
     }
-    public void show() { scene.setRoot(root); }
+    public TilePane generateBoard(int SIZE) {
+        TilePane gameBoard = new TilePane();
+        gameBoard.setAlignment(Pos.CENTER);
+        gameBoard.setVgap(2.5);
+        gameBoard.setHgap(2.5);
+        gameBoard.setMaxWidth(SIZE / 2);
+        for(int i = 0; i < 9; i++) {
+            StackPane stackPane = new StackPane();
+            int counter = i;
+            Rectangle rect = new Rectangle(SIZE / 6.5, SIZE / 6.5);
+            rect.setFill(Color.DIMGREY);
+            rect.setOnMouseClicked(event -> {
+                if(!gameSequence.isEmpty()) {
+                    if(counter == gameSequence.peekFirst()) {
+                        gameSequence.pop();
+                        if(gameSequence.isEmpty()) {
+                            score++;
+                            scoreLabel.setText("Score: " + score);
+                            gameLoop();
+                        }
+                    }
+                    else scoreLabel.setText("Game over!");
+                }
+            });
+            Label label = new Label();
+            label.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 40));
+            label.setTextFill(Color.CORNFLOWERBLUE);
+            label.setMouseTransparent(true);
+            labelsList.add(label);
+            rectList.add(rect);
+            stackPane.getChildren().addAll(rect, label);
+            gameBoard.getChildren().add(stackPane);
+        }
+        return gameBoard;
+    }
+    public void gameLoop() {
+        gameSequence.clear();
+        generateSequence();
+        for(int i = 0; i < gameSequence.size(); i++) {
+            int curr = gameSequence.get(i);
+            labelsList.get(curr).setText(Integer.toString(i + 1));
+            Timeline t = playAnimation(curr);
+            t.play();
+        }
+    }
+    public Timeline playAnimation(int curr) {
+        Timeline t = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                        new KeyValue(labelsList.get(curr).opacityProperty(), 100)),
+                new KeyFrame(Duration.seconds(3),
+                        new KeyValue(labelsList.get(curr).opacityProperty(), 0))
+        );
+        return t;
+    }
+    public void generateSequence() {
+        int rand;
+        for(int i = 0; i < score + 1; i++) {
+            rand = ThreadLocalRandom.current().nextInt(0,9);
+            if(gameSequence.contains(rand)) {
+                while(gameSequence.contains(rand)) {
+                    rand = ThreadLocalRandom.current().nextInt(0,9);
+                }
+                gameSequence.push(rand);
+            }
+            else gameSequence.push(rand);
+        }
+    }
+    public void show() { scene.setRoot(startScreen); }
 }
