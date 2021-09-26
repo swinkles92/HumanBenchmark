3ba12dc085a828f90ca734dc098ff1729b7432d8
Samuel Winkles <swinkles92@unm.edu>
2021-09-26 14:11:25 -0600

    Added Typing Game functionality.

--- src/TypingGame.java
+++ src/TypingGame.java
@@ -2,22 +2,29 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class TypingGame {
     private final Scene scene;
-    private final BorderPane root;
+    private final BorderPane startScreen;
+    private final BorderPane gameScreen;
+    private String paragraph = "The quick brown fox jumped over the lazy dog.";
+    private Text paragraphBox;
+    private int counter = 0;
 
     public TypingGame(Scene scene, int SIZE, TilePane mainMenu) {
         this.scene = scene;
-        root = new BorderPane();
+        startScreen = new BorderPane();
+        gameScreen = new BorderPane();
+        paragraphBox = new Text();
 
-        VBox vBox = new VBox(10);
-
+        VBox startVBox = new VBox(10);
         Label title = new Label("Typing");
         title.setFont(new Font(40));
         title.setAlignment(Pos.CENTER);
@@ -25,16 +32,44 @@ public class TypingGame {
                 "Click Start to begin.");
         gameDesc.setFont(new Font(20));
         gameDesc.setWrappingWidth(225);
-
         Button backButton = new Button("Back to Main Menu");
+        Button startButton = new Button("Start");
+        startVBox.getChildren().addAll(title, gameDesc, startButton, backButton);
+        startVBox.setAlignment(Pos.CENTER);
+        startScreen.setCenter(startVBox);
+
+        VBox gameVBox = new VBox(20);
+        paragraphBox.setText(paragraph);
+        paragraphBox.setFont(new Font(30));
+        TextField userInputField = new TextField();
+        userInputField.setMaxWidth(300);
+        gameVBox.getChildren().addAll(paragraphBox, userInputField, backButton);
+        gameVBox.setAlignment(Pos.CENTER);
+        gameScreen.setCenter(gameVBox);
+        userInputField.setOnKeyReleased(event -> {
+            if(!paragraph.isEmpty()) {
+                gameLoop(userInputField.getText().charAt(0));
+                userInputField.setText(" ");
+            }
+            else paragraphBox.setText("Your time: ");
+        });
+        startButton.setOnAction(event -> {
+            scene.setRoot(gameScreen);
+        });
         backButton.setOnAction(event -> {
             scene.setRoot(mainMenu);
         });
-
-        vBox.getChildren().addAll(title, gameDesc, backButton);
-        vBox.setAlignment(Pos.CENTER);
-
-        root.setCenter(vBox);
     }
-    public void show() { scene.setRoot(root); }
+    public void gameLoop(char userInputChar) {
+        char currChar = paragraph.charAt(0);
+        if(currChar == userInputChar) {
+            paragraph = paragraph.substring(1);
+            paragraphBox.setFill(Color.BLACK);
+        }
+        else {
+            paragraphBox.setFill(Color.DARKRED);
+        }
+        paragraphBox.setText(paragraph);
+    }
+    public void show() { scene.setRoot(startScreen); }
 }
